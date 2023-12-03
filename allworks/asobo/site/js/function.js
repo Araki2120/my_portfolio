73df8e4af47c554e57b7063854865031a6be9980
Araki2120 <144511024+Araki2120@users.noreply.github.com>
2023-12-04 02:33:44 +0900

    [add] work個別ページ・中身の追加

---
+++ allworks/asobo/site/js/function.js
@@ -0,0 +1,173 @@
+//gnavの設定
+const gnavLinks = document.querySelectorAll('.gnav__link');
+const gnav = document.querySelector('.gnav__nav');
+const mouseOver = document.querySelector('.mouseOver');
+const mousePointer = document.querySelector('.gnav__pointer');
+
+gnavLinks.forEach((gnavLink) => {
+    //マウスポインターの初期widthの設定
+    const firstNavWidth = gnavLinks[0].offsetWidth + 'px';
+    mousePointer.style.width = firstNavWidth;
+
+    //マウスが乗った時の処理
+    gnavLink.addEventListener('mouseover', () => {
+
+        const gnavWidth = gnavLink.offsetWidth;
+        const gnavLeft = gnav.getBoundingClientRect().left;
+        const gnavLinkLeft = gnavLink.getBoundingClientRect().left;
+
+        mousePointer.style.width = gnavWidth + 'px';
+        mousePointer.style.left = (gnavLinkLeft - gnavLeft) + 'px';
+
+        if (!gnavLink.classList.contains('mouseOver')) {
+            gnavLink.classList.add('mouseOver');
+            gnavLinks[0].classList.remove('mouseOver');
+        } else {
+            gnavLink.classList.remove('mouseOver');
+            gnavLinks[0].classList.add('mouseOver');
+        }
+    });
+
+    //マウスが離れた時の処理
+    gnavLink.addEventListener('mouseleave', () => {
+        gnavLink.classList.remove('mouseOver');
+        gnavLinks[0].classList.add('mouseOver');
+        mousePointer.style.left = '20px';
+        mousePointer.style.width = firstNavWidth;
+    });
+});
+
+
+//menu用の設定
+const menuOpenBtn = document.querySelector('.header__btn');
+const menuCloseBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('#menu');
+const menuCont = document.querySelector('.menu__container');
+
+menuOpenBtn.addEventListener('click', () => {
+    menu.classList.add('onMask');
+    menuCont.classList.add('viewMenu');
+});
+
+menuCloseBtn.addEventListener('click', () => {
+    menuCont.classList.remove('viewMenu');
+
+    //menuが消えてからマスクを消す
+    setTimeout(() => {
+        menu.classList.remove('onMask');
+    }, 500);
+});
+
+
+//mv・footer用設定
+const mv = document.querySelector('#mv');
+const mvHeight = mv.offsetHeight;
+const footer = document.querySelector('.footer');
+
+window.addEventListener('scroll', () => {
+    const ST = window.scrollY;
+
+    if (ST > mvHeight + 100) {
+        mv.classList.add('hideMv');
+        footer.classList.add('showFooter');
+    } else {
+        mv.classList.remove('hideMv');
+        footer.classList.remove('showFooter');
+    }
+
+});
+
+
+//textアニメーションの設定
+const textAnimationObserver = () => {
+    const targets = document.querySelectorAll('.target');
+
+    const options = {
+        root: null,
+        rootMargin: '-120px 0px',
+        threshold: 1,
+    };
+
+    const txtObserver = new IntersectionObserver(showTxt, options);
+
+    targets.forEach((target) => {
+        txtObserver.observe(target);
+    });
+
+    function showTxt(entries) {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('upTxt');
+            }
+        });
+    };
+};
+
+textAnimationObserver();
+
+//product Swiperアニメーションの設定
+const productObserver = () => {
+    const productsSwiper = document.querySelector('.swiper-pd');
+
+    const options = {
+        root: null,
+        rootMargin: '-200px 0px',
+    };
+
+    const slideObserver = new IntersectionObserver(showSlide, options);
+
+    slideObserver.observe(productsSwiper);
+
+    function showSlide(entries) {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                console.log('hi!');
+                productsSwiper.classList.add('swiperSlideIn');
+            }
+        });
+    };
+};
+
+productObserver();
+
+
+
+//画面幅を変更した時の設定
+let timer = false;
+let windowWidth = window.innerWidth;
+
+const resize = () => {
+    console.log('画面幅を変更しました');
+
+    //ユーザーのデバイスを調べる
+    let ua = navigator.userAgent;
+    if (!(ua.indexOf('iPhone') > 0 || ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0 || ua.indexOf('Mobile') > 0)) {
+        // PC向けの処理
+        window.addEventListener('resize', () => {
+            if (timer !== false) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(() => {
+                let currentWidth = window.innerWidth;
+                // 今と横幅が違う時だけ、リロードする
+                if (windowWidth !== currentWidth) {
+                    location.reload();
+                }
+                windowWidth = currentWidth;
+            }, 200);
+        });
+    } else {
+        //スマホ向けの処理
+        window.addEventListener('orientationchange', function () {
+            console.log('スマホ画面を倒しました');
+            if (timer !== false) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(function () {
+                location.reload();
+            }, 200);
+        });
+    }
+};
+
+resize();
---
+++ allworks/asobo/site/js/swiper.js
@@ -0,0 +1,95 @@
+//MV Swiper用設定
+const swiperMv = new Swiper('.swiper-mv', {
+
+    allowTouchMove: false,
+    loop: true,
+    effect: 'slide',
+    slidesPerView: 1,
+    centeredSlides: true,
+    slideActiveClass: 'swiper-mv__isActive',
+    allowTouchMove: false,
+    speed: 1000,//1秒かけて移動
+
+    autoplay: {
+        delay: 9000,//9秒経ったら移動
+        disableOnInteraction: false,
+    },
+
+    navigation: { //ボタン左右の矢印 小さいけどちゃんと押せるように
+        nextEl: '.swiper-mv__next',
+        prevEl: '.swiper-mv__prev'
+    },
+
+    pagination: {
+        el: '.swiper-mv__pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+
+    //タブレット・PCのみ数字のページネーション
+    breakpoints: {
+        768: {
+            pagination: {
+                type: 'fraction',
+            },
+        },
+    },
+});
+
+//Productsセクション Swiper用設定
+const swiperProducts = new Swiper('.swiper-pd', {
+
+    effect: 'slide',
+    slidesPerView: 2,
+    spaceBetween: 20,
+    breakpoints: {
+        768: {
+            slidesPerView: 3.5,
+        },
+    },
+
+    navigation: {
+        nextEl: '.swiper-pd__next',
+        prevEl: '.swiper-pd__prev'
+    },
+
+    pagination: {
+        el: '.swiper-pd__pagination',
+        type: 'fraction',
+    },
+
+});
+
+//Userセクション swiper設定
+const swiperUser = new Swiper('.swiper-us', {
+
+    loop: true,
+    effect: 'slide',
+    slidesPerView: 1.5,
+    spaceBetween: 20,
+    centeredSlides: true,
+    slideActiveClass: 'swiper-us__isActive',
+    speed: 700,
+    breakpoints: {
+        768: {
+            slidesPerView: 2.5,
+        },
+        1440: {
+            slidesPerView: 4,
+            spaceBetween: 100,
+        },
+    },
+
+    autoplay: {
+        delay: 2400,
+        desableOnInteraction: false,
+    },
+    on: { ////マッチメディアにする！
+        // slideChangeTransitionEnd: function () {
+        //     const i = this.snapIndex;
+        //     // console.log(i);
+        //     const pageNum = document.querySelector('.swiper-mv__circle span');
+        //     pageNum.textContent = `${i}/3`;//現在の枚数の表示
+        // }
+    }
+});
